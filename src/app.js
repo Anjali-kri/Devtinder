f0ad16f77b8d1a6b3e0fb9e8262e3797853f9484
Anjali singh <73298495+anjalibobbysingh1@users.noreply.github.com>
2025-08-14 01:33:03 +0530

    understanding route and sequence matters

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+
+app.use("/hello/abc/123", (req, res) => {
+    res.send( "hello abc 123");
+});
+app.use("/hello/abc", (req, res) => {
+    res.send( " hello abc");
+});
+app.use("/hello", (req, res) => {
+    res.send( "abc");
+});
+app.use("/home", (req, res) => {
+    res.send("home page");
+});
 app.use((req, res) => {
     res.send("hey, finally i started the node journey");
 })
