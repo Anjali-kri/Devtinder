fc52c7a572849fca96a2104c9f28f4033cecd8eb
Anjali singh <73298495+anjalibobbysingh1@users.noreply.github.com>
2026-04-14 23:15:12 +0530

    schema validation added

--- model/user.js
+++ model/user.js
@@ -2,10 +2,37 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-    firstName:{type: String},
-    lastName:{type: String},
-    email:{type: String, unique: true},
-    password:{type: String}
+    firstName:{
+        type: String,
+        required: true,
+        max: 50
+    },
+    lastName:{
+        type: String,
+        default: "",
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now }
+
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    gender:{
+        type: String,
+        validate: {
+            validator: function(value) {
+                return value === "male" || value === "female" || value === "other";
+            },
+            message: "Invalid gender"
+        }
+    }
 });
 
 module.exports = mongoose.model("User", userSchema);
